9ca0c4fc559dc8733351fb7e9bbc6b366dde845a
DeepakM025 <31158802+DeepakM025@users.noreply.github.com>
2020-06-10 19:40:36 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  var url = window.location.href;
+  var the_arr = url.split('/');
+  the_arr.pop();
+  the_arr = the_arr.join('/')
+  $(".navigatethis").on("click", function(e){
+    e.preventDefault();
+    var slug = $(this).attr("data-navigate");
+    window.location = the_arr +"/"+ slug;
+  })
+
+  $(".mobile_menu .btn").on("click", function(){
+    $(this).toggleClass('active');
+    $(this).toggleClass('not-active');
+    $(".nav_end").toggleClass("d-block")
+  })
+    $(".project_wrap").on("click", function(){
+      var el = $(this);
+      var elimg = el.children("img").attr("src");
+      $(".img_modal_wrap").children("img").attr("src", elimg);
+      $(".img_modal_wrap").removeClass("d-none");
+    });
+
+    $(".close_modal").on("click", function(){
+      $(".img_modal_wrap").addClass("d-none");
+    })
+
+    setTimeout(function () {
+      $("#manufacture_slow polyline").css(
+        "stroke",
+        "#bebebe"
+      );
+    }, 2000);
+
+    $(".mobile_menu .btn").on("click", function(){
+      $(this).toggleClass('active');
+      $(this).toggleClass('not-active');
+      $(".nav_end").toggleClass("d-block")
+    })
+
+    $(".work_wrap").on("click", function(){
+      var el = $(this);
+      var elimg = el.attr("data-imglink");
+      $(".img_modal_wrap").children("img").attr("src", elimg);
+      $(".img_modal_wrap").removeClass("d-none");
+    });
+
+    $(".close_modal").on("click", function(){
+      $(".img_modal_wrap").addClass("d-none");
+    })
+
+    $(".mobile_menu .btn").on("click", function(){
+    $(this).toggleClass('active');
+    $(this).toggleClass('not-active');
+    $(".nav_end").toggleClass("d-block")
+  })
+  $(".mobile_menu .btn").on("click", function(){
+    $(this).toggleClass('active');
+    $(this).toggleClass('not-active');
+    $(".nav_end").toggleClass("d-block")
+  })
+})
